b09f337bb22b23e543c58468b7dedb215ddd0750
Don Moreno <d.moreno@esis-italia.com>
2026-03-13 16:24:23 +0100

    refactor: RootLayout with BgScene, Navbar, CartDrawer, Footer
    - relative min-h-screen flex flex-col on root
    - BgScene at z-0
    - main at z-[1] via arbitrary Tailwind value
    - CartDrawer rendered at layout level for persistent DOM presence

--- frontend/src/components/layout/RootLayout.jsx
+++ frontend/src/components/layout/RootLayout.jsx
@@ -1,15 +1,31 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import Navbar     from './Navbar'
+import Footer     from './Footer'
+import BgScene    from '../decorative/BgScene'
+import CartDrawer from '../ui/CartDrawer'
 
 export default function RootLayout() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    // relative is required on the root to establish a stacking context
+    // so that z-index values on children work as expected.
+    <div className="relative min-h-screen flex flex-col">
+
+      {/* z-0: Fixed decorative background */}
+      <BgScene />
+
+      {/* z-[100]: Sticky navbar */}
       <Navbar />
-      <main className="flex-1">
+
+      {/* z-auto → effectively z-1 within this stacking context */}
+      <main className="relative flex-1 z-[1]">
         <Outlet />
       </main>
+
       <Footer />
+
+      {/* z-[699]/z-[700]: Drawer always in DOM for exit animation */}
+      <CartDrawer />
+
     </div>
   )
 }
